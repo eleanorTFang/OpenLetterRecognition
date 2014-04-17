57cae93ee2ee57b6b9f66f4a85476ca91b3650ec
Shuli <ikari12@hotmail.com>
2014-04-17 14:26:01 +0900

    Add C++ source files.

---
+++ simple_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This binary file need the OpenCV "WITH QT4".
+# Please compile opencv using cmake, and QITH_QT option is selected.
+
+./LetterRecognition ./sample1.jpg "ムーミン" 1 0 0 3000 1
